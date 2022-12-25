50ac4f5aaed8af40b72b8d220beaa9679f001291
Omer-Demirtas <gamega55834@gmail.com>
2022-12-26 02:07:00 +0300

    Timer added to main

--- main/main.ino
+++ main/main.ino
@@ -1,14 +1,19 @@
+#include <Time.h>
+#include <TimeAlarms.h>
 #include "Map.hpp";
 #include "Task.h";
 #include "DB.hpp";
+
+AlarmID_t myAlarm = 0;  // this will be the ID for the first allocated alarm
+
 
 /*
  * TODO: 
   * Create a project object and all of the function should be in that class.  
   * create a DB interface to abstract db operation
+  * create a schedular service to manage tasks
 */
 
-Map<String, int> m(10);
 DB db("asd");
 
 void initCard()
@@ -21,16 +26,15 @@ void initCard()
 /*
   Get the task list from web
 */
-void initializeTasks()
+void initTasks()
 { 
-   db.initDB();
-
    Task* tasks = db.getTasks();
-   for(int i = 0; i < 1; i++)
-    {
-      Serial.println(tasks[i].getTimeString());
-    }
    
+   for(int i = 0; i < db.getTaskSize(); i++)
+   {
+    scheduleTask(tasks[i], i, alarmEvent);
+  
+   }
 }
 
 /*
@@ -38,13 +42,47 @@ void initializeTasks()
 */
 void sendLivePing() { }
 
+void scheduleTask(Task task, int index, void (*func)())
+{
+  Serial.println(String(task.getHour()) + String(task.getMinute()) + String(task.getSecond()));
+  int ID = Alarm.alarmOnce(AlarmHMS(7,30,0), alarmEvent);
+  //int ID = Alarm.alarmOnce(AlarmHMS(task.getHour(),task.getMinute(), 0), func);
+  db.setTaskId(index, ID);
+}
+
+void alarmEvent()
+{
+  Serial.println("Alarm");
+  Serial.println("ASDASDASDSADAS");
+}
+
 void setup()
 {
+  setTime(7,29,55,1,1,10); // set time to 7:29:40am Jan 1 2010 
+
   initCard();
-  initializeTasks();
+  db.initDB();
+  initTasks();
 }
 
 void loop() 
 {
+  digitalClockDisplay();
+  Alarm.delay(1000);
+}
 
+
+void digitalClockDisplay() {
+  // digital clock display of the time
+  Serial.print(hour());
+  printDigits(minute());
+  printDigits(second());
+  Serial.println();
+}
+
+void printDigits(int digits) {
+  Serial.print(":");
+  if (digits < 10)
+    Serial.print('0');
+  Serial.print(digits);
 }
