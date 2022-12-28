dbfa3185e1fa2f81a17b624549c1610ef43ca1f3
Omer-Demirtas <gamega55834@gmail.com>
2022-12-28 19:29:37 +0300

    Engine updates

--- main/Engine.h
+++ main/Engine.h
@@ -14,7 +14,7 @@ class Engine : Stepper {
   
     Engine(int p1, int p2, int p3, int p4) : Stepper(2038, p1, p2, p3, p4)
     {
-      setSpeed(5);
+      setSpeed(15);
     };
     void run()
     {
