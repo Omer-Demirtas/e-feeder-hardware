517731a96416f34eaf027f36cf2b91148d3c00ee
Omer-Demirtas <gamega55834@gmail.com>
2025-01-19 20:06:03 +0300

    main classes updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,48 @@
+#include <time.h>
+#include <TimeAlarms.h>
 #include <Arduino.h>
+#include "Task.h"
 
-// put function declarations here:
-int myFunction(int, int);
+/*
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+void wifiConnection() {
+  WiFi.begin("SamsungS22", "ieyt9222");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("connected..! ");
+  Serial.print("Got IP: ");  Serial.println(WiFi.localIP());  
+}
+*/
+
+void alarmEvent() {
+  Serial.println("Alarm");
+}
+
+void initializeTasks() {
+  Alarm.alarmOnce(7,30,0, alarmEvent);
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Task task();
+
+  Serial.begin(9600);
+
+  //wifiConnection();
+
+  // set current time
+  setTime(7, 29, 40, 1, 1, 10);
+
+  initializeTasks();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Alarm.delay(1000);
+  
+  Serial.println(second());
 }
