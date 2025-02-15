addae299269c710af606392cc3d72fa4aee99c82
Omer-Demirtas <gamega55834@gmail.com>
2025-02-15 18:45:58 +0300

    Task class updated with timerId field

--- include/Task.h
+++ include/Task.h
@@ -1,14 +1,33 @@
 #ifndef TASK_H
 #define TASK_H
 
+#include <Arduino.h>
+
 class Task {
     private:
         long id;
+        String time;
+        uint8_t alarmId;
     public:
-        long getId() { return id; }
-        void setId(long id) { this->id = id; }   
+        Task(long id, String time) : id(id), time(time) {}
 
-    Task(long id) : id(id) {}
+        long getId() { return id; }
+        void setId(long id) { this->id = id; }
+        uint8_t getAlarmId() { return alarmId; }
+        void setAlarmId(uint8_t alarmId) { this->alarmId = alarmId; }
+        String getTime() { return time; }
+        void setTime(String time) { this->time = time; }
+    
+        int getHour() {
+            return time.substring(0, time.indexOf(':')).toInt();
+        }
+
+        int getMinute() {
+            int index = time.indexOf(':');
+            int secondIndex = time.indexOf(':', index + 1);
+            
+            return time.substring(index + 1, secondIndex).toInt();
+        }
 };
 
 #endif
