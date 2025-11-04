35edb648d90c15ac4a2380507d3d5d6dcd4c1385
Omer-Demirtas <omer.halil.dmrts@gmail.com>
2025-11-04 05:12:35 -0500

    Added BluetoothInterface & cpp impl

---
+++ include/BluetoothInterface.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "CommunicationInterface.h"
+#include <BluetoothSerial.h>
+
+class BluetoothInterface : public CommunicationInterface {
+public:
+    BluetoothInterface(const char* deviceName);
+    void begin() override;
+    void update() override;
+    bool commandAvailable() override;
+    String readCommand() override;
+    void sendResponse(const String& response) override;
+
+private:
+    BluetoothSerial _serialBT;
+    String _deviceName;
+};
