37b8f905845a3334fd41888437a6874ef3a0e2a6
Omer-Demirtas <omer.halil.dmrts@gmail.com>
2025-11-04 05:34:08 -0500

    Updated feeder interface & servo and stepper impl

--- src/ServoFeeder.cpp
+++ src/ServoFeeder.cpp
@@ -1,29 +1,69 @@
 #include <Arduino.h>
 #include "ServoFeeder.h"
+#include <TimeLib.h>
 
-ServoFeeder::ServoFeeder(int servoPin) : pin(servoPin) {}
+ServoFeeder::ServoFeeder(int pin, int startAngle, int feedAngle) {
+    _pin = pin;
+    _startAngle = startAngle;
+    _feedAngle = feedAngle;
+    
+    Serial.println("Servo feed constructor");
+
+    Serial.println(startAngle);
+
+    _servo.write(startAngle);
+
+    // Initialize state
+    _state = IDLE;
+    _dispenseDuration = 3000;
+}
 
 void ServoFeeder::init() {
-    servo.attach(pin);
+    _servo.attach(_pin);
+     // Move to idle position on startup
+    _servo.write(_startAngle);
+    Serial.println("ServoFeeder Initialized.");
 }
 
-void ServoFeeder::feed() {
-    servo.write(180);
-
-    delay(1000);
-
-    servo.write(0);
+bool ServoFeeder::isBusy() {
+    return _state != IDLE;
 }
 
-void ServoFeeder::startFeed(int rotation) {
-    if (rotation > 0)
-    {
-        servo.write(180);
-    } else {
-        servo.write(0);
+void ServoFeeder::startDispensing(int amount) {
+    if (!isBusy()) {
+        _state = DISPENSING;
+        _startTime = millis(); 
+        _servo.write(_feedAngle);
+        
+        Serial.println("Feeder (Servo) started dispensing.");
+
+        Serial.print(hour());
+        Serial.print(":");
+        Serial.print(minute());
+        Serial.print(":");
+        Serial.print(second());
+        Serial.println("");
     }
 }
 
-void ServoFeeder::stopFeed() {
-    servo.write(90);
+void ServoFeeder::update() {
+    if (!isBusy()) {
+        return;
+    }
+
+    // Check if the dispensing duration has passed
+    if (millis() - _startTime >= _dispenseDuration) {
+         // Return servo to idle angle & set state to idle
+        _servo.write(_startAngle);
+        
+
+        Serial.println("Feeder (Servo) started dispensing.");
+
+        Serial.print(hour());
+        Serial.print(":");
+        Serial.print(minute());
+        Serial.print(":");
+        Serial.print(second());
+        Serial.println("");
+    }
 }
