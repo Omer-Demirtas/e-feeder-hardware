2520022b642858907cd2c8060501877bdd574477
Omer-Demirtas <omer.halil.dmrts@gmail.com>
2025-11-09 11:19:36 -0500

    Added new inherit LogObserver to CommunicationInterface  & impl isConnected & onLog methods

--- include/BluetoothInterface.h
+++ include/BluetoothInterface.h
@@ -10,7 +10,10 @@ public:
     bool commandAvailable() override;
     String readCommand() override;
     void sendResponse(const String& response) override;
-
+    
+    // Log Observer
+    bool isConnected() override;
+    void onLog(const String& formattedMessage) override;
 private:
     BluetoothSerial _serialBT;
     String _deviceName;
--- include/CommunicationInterface.h
+++ include/CommunicationInterface.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <Arduino.h>
+#include "LogObserver.h"
 
-class CommunicationInterface {
+class CommunicationInterface : public LogObserver {
 public:
     virtual ~CommunicationInterface() {}
 
@@ -10,4 +11,8 @@ public:
     virtual bool commandAvailable() = 0;
     virtual String readCommand() = 0;
     virtual void sendResponse(const String& response) = 0;
+
+    // Log observer
+    virtual void onLog(const String& formattedMessage) override = 0;
+    virtual bool isConnected() override = 0;
 };
